a368bf221deb5aa9aab9206f591ba24dcff0b464
Tyler Norman <TylerNorman881@hotmail.com>
2018-10-24 17:47:32 -0400

    Photo Screengrab, Skybox Testing
    Implemented ability to take a snapshot of the current view using the camera (LMB). Testing out skybox dimensions.

--- Assets/Scripts/Player/PlayerScript.cs
+++ Assets/Scripts/Player/PlayerScript.cs
@@ -6,11 +6,20 @@ using DG.Tweening;
 
 public class PlayerScript : MonoBehaviour
 {
+    [Header("Camera Flash Properties")]
     [SerializeField] private Image cameraUI;
     [SerializeField] private Image cameraFlash;
 
     [SerializeField] private float flashAlpha = 0.95f;
     [SerializeField] private float flashDelay = 0.25f;
+
+    [SerializeField] private Camera playerCam;
+
+    [Header("Snapshot Test")]
+    [SerializeField] private Image photoPreview;
+
+    [Header("UI Canvas")]
+    [SerializeField] private Canvas canvas;
 
     private Color hideFlash = new Color(0, 0, 0, 0);
     private Color showFlash;
@@ -20,9 +29,9 @@ public class PlayerScript : MonoBehaviour
     {
         showFlash = new Color(1, 1, 1, flashAlpha);
         cameraFlash.color = hideFlash;
+        photoPreview.enabled = false;
 
-        TakePhoto();
-	}
+    }
 	
 	// Update is called once per frame
 	void Update ()
@@ -31,10 +40,48 @@ public class PlayerScript : MonoBehaviour
         {
             TakePhoto();
         }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            //TODO: Add the Menu here
+            Debug.Break();
+        }
     }
 
     private void TakePhoto()
     {
+        //Try to remove the camera from the shot
+        canvas.enabled = false;
+
+        //Render out the screenshot
+        int currentCamPosX = (int)gameObject.transform.position.x;
+        int currentCamPosY = (int)gameObject.transform.position.y;
+
+        RenderTexture renderSpace = new RenderTexture(Screen.width, Screen.height, 24);
+
+        playerCam.targetTexture = renderSpace;
+        Texture2D snapshotTexture = new Texture2D(Screen.width, Screen.height, TextureFormat.RGBA32, true);
+        playerCam.Render();
+        RenderTexture.active = renderSpace;
+
+        //ReadPixels from the top-left to the bottom-right of the screen
+        snapshotTexture.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+
+        snapshotTexture.filterMode = FilterMode.Point;
+        snapshotTexture.Apply(); //Make sure to Apply() so the texture is temp-saved
+        playerCam.targetTexture = null;
+        RenderTexture.active = null; //notes say to set this to avoid errors
+
+        Sprite returnSnapshot = Sprite.Create(snapshotTexture, new Rect(0, 0, snapshotTexture.width, snapshotTexture.height), new Vector2(0.5f, 0.5f), 1);
+
+        photoPreview.sprite = returnSnapshot;
+
+        canvas.enabled = true;
+
+        if (photoPreview.enabled == false)
+            photoPreview.enabled = true;
+
+        //Animate the camera
         cameraFlash.color = showFlash;
 
         Tween cameraTween = cameraFlash.DOColor(hideFlash, 0.5f)
--- Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/RigidbodyFirstPersonController.cs
+++ Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/RigidbodyFirstPersonController.cs
@@ -13,15 +13,11 @@ namespace UnityStandardAssets.Characters.FirstPerson
             public float ForwardSpeed = 8.0f;   // Speed when walking forward
             public float BackwardSpeed = 4.0f;  // Speed when walking backwards
             public float StrafeSpeed = 4.0f;    // Speed when walking sideways
-            public float RunMultiplier = 2.0f;   // Speed when sprinting
-	        public KeyCode RunKey = KeyCode.LeftShift;
+            //public float RunMultiplier = 2.0f;   // Speed when sprinting
             public float JumpForce = 30f;
             public AnimationCurve SlopeCurveModifier = new AnimationCurve(new Keyframe(-90.0f, 1.0f), new Keyframe(0.0f, 1.0f), new Keyframe(90.0f, 0.0f));
             [HideInInspector] public float CurrentTargetSpeed = 8f;
 
-#if !MOBILE_INPUT
-            private bool m_Running;
-#endif
 
             public void UpdateDesiredTargetSpeed(Vector2 input)
             {
@@ -42,25 +38,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
 					//handled last as if strafing and moving forward at the same time forwards speed should take precedence
 					CurrentTargetSpeed = ForwardSpeed;
 				}
-#if !MOBILE_INPUT
-	            if (Input.GetKey(RunKey))
-	            {
-		            CurrentTargetSpeed *= RunMultiplier;
-		            m_Running = true;
-	            }
-	            else
-	            {
-		            m_Running = false;
-	            }
-#endif
             }
-
-#if !MOBILE_INPUT
-            public bool Running
-            {
-                get { return m_Running; }
-            }
-#endif
         }
 
 
@@ -104,18 +82,6 @@ namespace UnityStandardAssets.Characters.FirstPerson
             get { return m_Jumping; }
         }
 
-        public bool Running
-        {
-            get
-            {
- #if !MOBILE_INPUT
-				return movementSettings.Running;
-#else
-	            return false;
-#endif
-            }
-        }
-
 
         private void Start()
         {
@@ -127,11 +93,6 @@ namespace UnityStandardAssets.Characters.FirstPerson
 
         private void Update()
         {
-            if (Input.GetKeyDown(KeyCode.Escape) )
-            {
-                Debug.Break();
-            }
-
             RotateView();
 
             if (Input.GetButtonDown("Jump") && !m_Jump)
